57a999cef37429e268c0b071749b8dde6a5278cf
Jose Jacinto <jacinto.jd@gmail.com>
2019-10-04 10:33:17 +0100

    Version bump

--- cronjob.sh
+++ cronjob.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 echo "\n------------" &&
 echo "$(date)\n" &&
-cd ~/mf-music &&
 pm2 stop 0 &&
 pm2 delete 0 &&
 mpc stop &&
-rm -rf mf-music &&
 cd ~ &&
+rm -rf mf-music &&
 git clone https://github.com/jose-jacinto/mf-music.git &&
 #sudo apt-get update &&
 #sudo apt-get install -y mpv &&
