71b11322536a207b5a5e3e6f7c4a8f5236e27389
Jose Jacinto <jacinto.jd@gmail.com>
2019-10-05 14:51:38 +0100

    Minor change to cronjob

--- cronjob.sh
+++ cronjob.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
-echo "\n------------" &&
-echo "$(date)\n" &&
-pm2 stop 0 &&
-pm2 delete 0 &&
-mpc stop &&
-cd ~ &&
-rm -rf mf-music &&
-git clone https://github.com/jose-jacinto/mf-music.git &&
-#sudo apt-get update &&
-#sudo apt-get install -y mpv &&
-cd ~/mf-music &&
-npm install &&
-git checkout package-lock.json &&
-pm2 start index.js &&
+echo "\n------------"
+echo "$(date)\n"
+pm2 stop 0
+pm2 delete 0
+mpc stop
+cd ~
+rm -rf mf-music
+git clone https://github.com/jose-jacinto/mf-music.git
+#sudo apt-get update
+#sudo apt-get install -y mpv
+cd ~/mf-music
+npm install
+git checkout package-lock.json
+pm2 start index.js
 pm2 save
