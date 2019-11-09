e074190cc6b04817510c6a7fd53aa821704511be
Jose Jacinto <jacinto.jd@gmail.com>
2019-11-09 14:41:10 +0000

    Changed buffering, caching and logging

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const https = require('https');
-const Lecmd = require('node-cmd');
+const cmdEngine = require('node-cmd');
 const fs = require('fs');
 const path = require('path');
 const { spawn } = require('child_process');
@@ -9,6 +9,8 @@ const getSize = require('get-folder-size');
 
 const default_volume = 75;
 const spot_volume = 95;
+const version = '1.0.1';
+const MaxSizeAllowedBytes = 4000000000; // 4000000000 == 4Gb
 
 const mpd = require('mpd'),
   cmd = mpd.cmd
@@ -16,12 +18,24 @@ const mpd = require('mpd'),
   port: 6600,
   host: 'localhost',
 });
+const pharmacy = require('/boot/pharmacy.json');
+
+let winston = require('winston');
+let {Loggly} = require('winston-loggly-bulk');
+
+
+winston.add(new Loggly({
+    token: "eef2d315-5ede-4cbd-b82c-8638bbdfb792",
+    subdomain: "maisfarmacia",
+    tags: ["Winston-NodeJS"],
+    json: true
+}));
 
 mpdClient.on('ready', function() {
-  console.log("MPD Client is Ready");
+  winston.log('info', `MPD Client is Ready with version : ${version}`, { labels: [`${pharmacy.ANF}`, 'radio'] });
 });
 
-const pharmacy = require('/boot/pharmacy.json');
+
 
 const socket = io( (pharmacy.env === "PROD") ? 'https://servicos.maisfarmacia.org' : 'http://192.168.2.102:9012', { path: '/piradio' });
 console.log(  (pharmacy.env === "PROD") ? 'Started In PROD' : 'Started In DEV' )
@@ -82,11 +96,17 @@ let deleteAll = false;
 fs.stat('../cache', (err) => {
   if (!err) {
     getSize('../cache', (err, size) => {
+
+      winston.log('info', 'Checked Cache - ' + (size / 1024 / 1024).toFixed(2) + ' MB Occupied.', { labels: [`${pharmacy.ANF}`, 'radio', 'cache-status'] });
+      console.log((size / 1024 / 1024).toFixed(2) + ' MB Occupied');
+      console.log((MaxSizeAllowedBytes / 1024 / 1024).toFixed(2) + ' MB Limit')
+
       if (err) {
         console.error(err);
         process.exit();
-      } else if (size > 4000000000) {
+      } else if (size > MaxSizeAllowedBytes) {
         deleteAll = true;
+        winston.log('info', 'Deleting All Cache', { labels: [`${pharmacy.ANF}`, 'radio',  'cache-status'] });
       }
     });
   }
@@ -109,6 +129,7 @@ fs.stat('../cache', (err) => {
             if (deleteAll || now.getMonth() - birthtime.getMonth() > 0) {
               try {
                 fs.unlinkSync(file);
+                console.log('Unlinked ' + file)
               } catch (error) {
                 console.error(error);
               }
@@ -182,45 +203,46 @@ mpdClient.on('system-player', function() {
   });
 });
 
-setInterval( () => {
+setInterval(() => {
   if(changesBuffer !== null) {
-    console.log(`Changed to a new song ${changesBuffer}`);
+    let songUrl = changesBuffer;
 
-    if (changesBuffer.indexOf('Spot_') !== -1) {
-      Lecmd.run(`mpc volume ${spot_volume}`);
+    changesBuffer = null;
+    console.log(`Changed to a new song ${songUrl}`);
+    winston.log('info', 'New Song Playing - ' + songUrl, { labels: [`${pharmacy.ANF}`, 'radio',  'music-status'] });
+
+    if (songUrl.indexOf('Spot_') !== -1) {
+      cmdEngine.run(`mpc volume ${spot_volume}`);
       console.log(`Changed to volume ${spot_volume} playing a Spot`)
     } else {
-      Lecmd.run(`mpc volume ${default_volume}`);
+      cmdEngine.run(`mpc volume ${default_volume}`);
      // console.log(`Changed to volume ${default_volume}`)
     }
 
-    socket.emit('playing', pharmacy.ANF, { src: changesBuffer});
+    socket.emit('playing', pharmacy.ANF, { src: songUrl});
 
-    if (changesBuffer.indexOf('http') !== -1) {
+    if (songUrl.indexOf('http') !== -1) {
       fs.stat('../cache', (err) => {
         if (err) {
           fs.mkdirSync('../cache');
         }
-  
         getSize('../cache', async (err, size) => {
           if (err) {
             console.error(err);
-          } else  if (size < 4000000000) {
-            console.log('Have space for new music in cache : TRUE');
-            console.log(`Caching ${changesBuffer}`);
-            const playlistDir = `../cache/${changesBuffer.split('/')[4]}`;
+          } else  if (size < MaxSizeAllowedBytes) {
+            console.log(`Caching ${songUrl}`);
+            const playlistDir = `../cache/${songUrl.split('/')[4]}`;
             if (!fs.existsSync(playlistDir)) fs.mkdirSync(playlistDir); // Created the directory if not exist
   
-            const songFile = fs.createWriteStream(`${playlistDir}/${changesBuffer.split('/')[5]}`); //Write it now
-            httpGet(changesBuffer)
+            const songFile = fs.createWriteStream(`${playlistDir}/${songUrl.split('/')[5]}`); //Write it now
+            httpGet(songUrl)
               .then(response => {
+                winston.log('info', 'Cached Song - ' + songUrl, { labels: [`${pharmacy.ANF}`, 'radio',  'cache-status'] });
                 response.pipe(songFile);
-                changesBuffer = null;
               })
               .catch((err) => {
-                fs.unlinkSync(`${playlistDir}/${changesBuffer.split('/')[5]}`);
+                fs.unlinkSync(`${playlistDir}/${songUrl.split('/')[5]}`);
                 console.error(err);
-                changesBuffer = null;
                 if (err.message.indexOf('Failed') === -1) {
                   process.exit();
                 }
@@ -232,15 +254,16 @@ setInterval( () => {
       changesBuffer = null;
     }
   }
-},500)
+},1000)
 
 // event: on connect
 socket.on('connect', () => {
   console.log(`${pharmacy.ANF}: Connected to main server, setting defaults`);
+  winston.log('info', 'Successfully Connected to BE', { labels: [`${pharmacy.ANF}`, 'radio',  'connection-status'] });
   connected = true;
-  Lecmd.run(`mpc volume ${default_volume}`);
-  Lecmd.run(`mpc random off`);
-  Lecmd.run(`mpc repeat on`);
+  cmdEngine.run(`mpc volume ${default_volume}`);
+  cmdEngine.run(`mpc random off`);
+  cmdEngine.run(`mpc repeat on`);
   
   if (interval !== null) {
     clearInterval(interval);
@@ -253,6 +276,7 @@ socket.on('connect', () => {
 // event: on disconnect
 socket.on('disconnect', () => {
   console.log(`${pharmacy.ANF}: Disconnected from server. Trying to reconnect...`);
+  winston.log('info', 'Disconnected from BE', { labels: [`${pharmacy.ANF}`, 'radio',  'connection-status'] });
   connected = false;
 
   // try to reconnect to the server
@@ -266,6 +290,8 @@ socket.on('disconnect', () => {
 
 // event: on play
 socket.on('play', (msg) => {
+
+  winston.log('info', 'Got request to PLAY', { labels: [`${pharmacy.ANF}`, 'radio',  'connection-status'] });
 
   mpdClient.sendCommand(cmd("clear", []), function(err, result) {
     if (err) throw err;
@@ -285,27 +311,34 @@ socket.on('play', (msg) => {
 
   msg.playlistLocal.forEach((url, index) => {
     try {
+  
       if (fs.statSync(url).size === 0) { // if the file does not exist or is empty
         console.log(`# Added song to playlist (from S3) : ${msg.playlist[index]}`);
       } else {
         console.log(`# Added song to playlist (from cache) : ${path.resolve(__dirname,url.replace(/(\s+)/g, '\\$1'))}`)
         finalPlaylist.push(url);
+        //winston.log('info', 'Pushed ' + finalPlaylist.length + ' Songs to queue', { labels: [`${pharmacy.ANF}`, 'radio',  'music-status'] });
 
-        Lecmd.run(`mpc add ${path.resolve(__dirname, url).replace(/(\s+)/g, '\\$1')}`);
+        cmdEngine.run(`mpc add ${path.resolve(__dirname, url).replace(/(\s+)/g, '\\$1')}`);
       }
-    } catch (err) { // don't know why try catch. Find out and leave comment with reason why
+
+    } catch (err) {// File not found so it throws error
+
+
       finalPlaylist.push(msg.playlist[index]);
       mpdClient.sendCommand(cmd("add", [msg.playlist[index].replace(/\s/g, '+')]), function(err, result) {
         if (err) throw err;
       });
     }
   });
-  Lecmd.run(`mpc play`); // play the playlist now
+  cmdEngine.run(`mpc play`); // play the playlist now
 
 });
 
 // event: on stop
 socket.on('stop', () => {
+
+  winston.log('info', 'Got request to STOP', { labels: [`${pharmacy.ANF}`, 'radio',  'connection-status'] });
   if (!playing) {
     console.log(`${pharmacy.ANF}: received request to stop from server, but was already stopped`);
   } else {
@@ -323,6 +356,7 @@ socket.on('stop', () => {
 
 // event: on pause
 socket.on('pause', () => {
+  winston.log('info', 'Got request to PAUSE', { labels: [`${pharmacy.ANF}`, 'radio',  'connection-status'] });
   console.log(`${pharmacy.ANF}: received request to pause from server`);
   paused = true;
   mpdClient.sendCommand(cmd("pause", []), function(err, result) {
@@ -344,6 +378,7 @@ socket.on('resume', () => {
 
 // event: on next
 socket.on('next', () => {
+  winston.log('info', 'Got request to NEXT', { labels: [`${pharmacy.ANF}`, 'radio',  'connection-status'] });
   console.log(`${pharmacy.ANF}: received request to skip from server`);
   mpdClient.sendCommand(cmd("next", []), function(err, result) {
     if (err) throw err;
@@ -353,10 +388,12 @@ socket.on('next', () => {
 
 // comments that applied to "socket.on('play')" are applied here
 socket.on('shuffle', (msg) => {
+
+  winston.log('info', 'Got request to SHUFFLE', { labels: [`${pharmacy.ANF}`, 'radio',  'connection-status'] });
   if (playing) {
     console.log(`${pharmacy.ANF}: received request to play from server. Restarting playlist or playing new one from the beginning`); 
   } else {
     console.log(`${pharmacy.ANF}: received request to play from server`);
   }
-  Lecmd.run(`mpc play`);
+  cmdEngine.run(`mpc play`);
 });
