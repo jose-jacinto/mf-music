93d085faa5969da12f54d9527fbdd877c4beb893
Jose Jacinto <jacinto.jd@gmail.com>
2020-03-05 20:24:55 +0000

    Changed Service url

--- post_update.sh
+++ post_update.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 echo "Started Running Post Update Script"
 
-sudo raspi-config --expand-rootfs
-sudo reboot
+#sudo raspi-config --expand-rootfs
+#sudo reboot
 
 echo "Finished Running Post Update Script"
 exit 0
